a2fe6fd3c2a17dc918313bdbef8d4a4f9f776307
verseboys <www@qq.com>
2018-03-23 17:17:10 +0800

    初始化。清理过时url

--- app/src/main/java/com/jhsc/testwebservice/MainActivity.java
+++ app/src/main/java/com/jhsc/testwebservice/MainActivity.java
@@ -3,6 +3,7 @@ package com.jhsc.testwebservice;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -59,6 +60,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        queryButton = (Button) findViewById(R.id.query_btn1);
+
+        queryButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 手机号码（段）
+                String phoneSec = phoneSecEditText.getText().toString().trim();
+                // 简单判断用户输入的手机号码（段）是否合法
+                if ("".equals(phoneSec) || phoneSec.length() < 7) {
+                    // 给出错误提示
+                    phoneSecEditText.setError("您输入的手机号码（段）有误！");
+                    phoneSecEditText.requestFocus();
+                    // 将显示查询结果的TextView清空
+                    resultView.setText("");
+                    return;
+                }
+
+                //启动后台异步线程进行连接webService操作，并且根据返回结果在主线程中改变UI
+                QueryAddressTask queryAddressTask = new QueryAddressTask();
+                //启动后台任务
+                queryAddressTask.execute(phoneSec);
+
+            }
+        });
+
+
+        toBase64();
     }
 
     /**
@@ -66,9 +95,9 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param phoneSec 手机号段
      */
-    public String getRemoteInfo(String phoneSec) throws Exception{
-      //  http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL
-       // String WSDL_URI = "http://webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL";//wsdl 的uri
+    public String getRemoteInfo(String phoneSec) throws Exception {
+        //  http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL
+        // String WSDL_URI = "http://webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL";//wsdl 的uri
         String WSDL_URI = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL";//wsdl 的uri
         String namespace = "http://WebXml.com.cn/";//namespace
         String methodName = "getMobileCodeInfo";//要调用的方法名称
@@ -77,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
         // 设置需调用WebService接口需要传入的两个参数mobileCode、userId
         request.addProperty("mobileCode", phoneSec);
         request.addProperty("userId", "");
+
 
         //创建SoapSerializationEnvelope 对象，同时指定soap版本号(之前在wsdl中看到的)
         SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapSerializationEnvelope.VER12);
@@ -90,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         SoapObject object = (SoapObject) envelope.bodyIn;
         // 获取返回的结果
         result = object.getProperty(0).toString();
-        Log.d("debug",result);
+        Log.d("debug", result);
         return result;
 
     }
@@ -116,5 +146,47 @@ public class MainActivity extends AppCompatActivity {
             resultView.setText(result);
         }
     }
+
+    public void toBase64() {
+        String encodedString = Base64.encodeToString("whoislcj".getBytes(), Base64.DEFAULT);
+        Log.e("Base64", "Base64---->" + encodedString);
+    }
+
+
+    /**
+     * 手机号段归属地查询
+     *
+     * @param phoneSec 手机号段
+     */
+    public String getRemoteInfoEme(String phoneSec) throws Exception {
+        //  http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL
+        // String WSDL_URI = "http://webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL";//wsdl 的uri
+        String WSDL_URI = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx?WSDL";//wsdl 的uri
+        String namespace = "http://WebXml.com.cn/";//namespace
+        String methodName = "getMobileCodeInfo";//要调用的方法名称
+
+        SoapObject request = new SoapObject(namespace, methodName);
+        // 设置需调用WebService接口需要传入的两个参数mobileCode、userId
+        request.addProperty("mobileCode", phoneSec);
+        request.addProperty("userId", "");
+
+        //创建SoapSerializationEnvelope 对象，同时指定soap版本号(之前在wsdl中看到的)
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapSerializationEnvelope.VER12);
+        envelope.bodyOut = request;//由于是发送请求，所以是设置bodyOut
+        envelope.dotNet = true;//由于是.net开发的webservice，所以这里要设置为true
+
+        HttpTransportSE httpTransportSE = new HttpTransportSE(WSDL_URI);
+        httpTransportSE.call(null, envelope);//调用
+
+        // 获取返回的数据
+        SoapObject object = (SoapObject) envelope.bodyIn;
+        // 获取返回的结果
+        result = object.getProperty(0).toString();
+        Log.d("debug", result);
+        return result;
+
+    }
+
+
 }
 
